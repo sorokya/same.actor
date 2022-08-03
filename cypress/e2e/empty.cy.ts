dc019ed970f501b726b2b71518181b3cd08d0d64
Richard Leek <richard@richardleek.com>
2022-08-03 21:50:24 +0200

    add empty test

---
+++ cypress/e2e/empty.cy.ts
@@ -0,0 +1,6 @@
+describe("empty", () => {
+    it("should pass", () => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        expect(true).to.be.true;
+    });
+});
